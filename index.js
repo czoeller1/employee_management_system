1408083288f2b7def452b00ae7b5fb07cc7ac5c3
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-24 23:55:27 -0700

    added all base functionality

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
-// console.table([
-//   {
-//     name: "foo",
-//     age: 10,
-//   },
-//   {
-//     name: "bar",
-//     age: 20,
-//   },
-// ]);
-
 const connection = mysql.createConnection({
   host: "localhost",
 
